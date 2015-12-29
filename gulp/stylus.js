88881b1c67e7e7cf93548218046e084966244eac
floz <florian.zumbrunn@gmail.com>
2015-12-29 11:21:33 +0100

    styling + change gulp

--- gulp/stylus.js
+++ gulp/stylus.js
@@ -6,6 +6,8 @@ var nib		= require('nib');
 
 gulp.task( 'stylus', function() {
 	return gulp.src( path.stylus+'main.styl' )
+		.pipe( plumber() )
 		.pipe(stylus({use:[nib()], sourcemap:{inline: true}}))
+		.pipe( plumber.stop() )
 		.pipe(gulp.dest(path.build+'css/'))
 } );
