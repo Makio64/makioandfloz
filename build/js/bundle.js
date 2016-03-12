1335cba666112aaec0730fc3341f03bf9de026c0
floz <florian.zumbrunn@gmail.com>
2016-03-11 22:58:19 -0800

    bouboup modifs

--- build/js/bundle.js
+++ build/js/bundle.js
@@ -73,7 +73,7 @@
 /******/ 			script.charset = 'utf-8';
 /******/ 			script.async = true;
 
-/******/ 			script.src = __webpack_require__.p + "" + chunkId + "." + {"1":"47a10173a633d5b9ed71"}[chunkId] + ".bundle.js";
+/******/ 			script.src = __webpack_require__.p + "" + chunkId + "." + {"1":"55bb379a105a1b155500"}[chunkId] + ".bundle.js";
 /******/ 			head.appendChild(script);
 /******/ 		}
 /******/ 	};
@@ -85,7 +85,7 @@
 /******/ 	__webpack_require__.c = installedModules;
 
 /******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "./js/";
+/******/ 	__webpack_require__.p = "/js/";
 
 /******/ 	// Load entry module and return exports
 /******/ 	return __webpack_require__(0);
