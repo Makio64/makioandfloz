9018e24adeea9160bd18a22b7794b81cef304299
floz <florian.zumbrunn@gmail.com>
2016-03-28 22:15:44 -0700

    ogs

--- static/ogs/deer-xmas.php
+++ static/ogs/deer-xmas.php
@@ -1,12 +1,12 @@
 <!--  ARTICLE VALUES -->
-<meta property="og:title" content="Case study of the treelines experiments" />
-<meta property="og:description" content="Making of the treelines experiments using meshline" />
-<meta property="og:url" content="http://www.makioandfloz.com/articles/treeline/" />
-<meta property="og:image" content="http://www.makioandfloz.com/articles/treeline/facebook.jpg" />
+<meta property="og:title" content="Case study of the christmas experiment Deer Xmas" />
+<meta property="og:description" content="Making of Deer Xmas - Create an easy 3D model with lines, based on MeshLines" />
+<meta property="og:url" content="http://www.makioandfloz.com/articles/deer-xmas/" />
+<meta property="og:image" content="http://www.makioandfloz.com/articles/deer-xmas/facebook.jpg" />
 
-<meta name="twitter:creator" content="@makio64" />
-<meta name="twitter:title" content="Case study of the treelines experiments" />
-<meta name="twitter:description" content="Making of the treelines experiments using meshline" />
-<meta name="twitter:image" content="http://www.makioandfloz.com/articles/treeline/twitter.jpg" />
+<meta name="twitter:creator" content="@florianzumbrunn" />
+<meta name="twitter:title" content="Case study of the christmas experiment Deer Xmas" />
+<meta name="twitter:description" content="Making of Deer Xmas - Create an easy 3D model with lines, based on MeshLines" />
+<meta name="twitter:image" content="http://www.makioandfloz.com/articles/deer-xmas/twitter.jpg" />
 
-<title>Case study of the treelines experiments - Makio and Floz - Blog about art & dev</title>
+<title>Making of Deer Xmas - Makio and Floz - Blog about art & dev</title>
