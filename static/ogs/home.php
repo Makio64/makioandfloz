171888bcb38e5b9d0f7bfc2e6e273673999f18e0
Ronai David <davronai@gmail.com>
2016-03-29 13:26:01 +0900

    update for metatag + php

---
+++ static/ogs/home.php
@@ -0,0 +1,12 @@
+<!--  ARTICLE VALUES -->
+<meta property="og:title" content="makio & floz" />
+<meta property="og:description" content="Blog about art & dev" />
+<meta property="og:url" content="http://www.makioandfloz.com" />
+<meta property="og:image" content="http://www.makioandfloz.com/facebook.jpg" />
+
+<meta name="twitter:creator" content="@makio64" />
+<meta name="twitter:title" content="makioandfloz" />
+<meta name="twitter:description" content="Blog about art & dev" />
+<meta name="twitter:image" content="http://www.makioandfloz.com/twitter.jpg" />
+
+<title>Makio and Floz - Blog about art & dev</title>
---
+++ static/ogs/treeline.php
@@ -0,0 +1,12 @@
+<!--  ARTICLE VALUES -->
+<meta property="og:title" content="Case study of the treelines experiments" />
+<meta property="og:description" content="Making of the treelines experiments using meshline" />
+<meta property="og:url" content="http://www.makioandfloz.com/articles/treeline/" />
+<meta property="og:image" content="http://www.makioandfloz.com/articles/treeline/facebook.jpg" />
+
+<meta name="twitter:creator" content="@makio64" />
+<meta name="twitter:title" content="Case study of the treelines experiments" />
+<meta name="twitter:description" content="Making of the treelines experiments using meshline" />
+<meta name="twitter:image" content="http://www.makioandfloz.com/articles/treeline/twitter.jpg" />
+
+<title>Case study of the treelines experiments - Makio and Floz - Blog about art & dev</title>
